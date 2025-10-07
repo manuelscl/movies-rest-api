fb289e4370e2e88f99b8c4783723382a8c26bb5c
manuelscl <manuelscl.dev@gmail.com>
2025-10-06 21:34:44 -0600

    fix: add manual CORS handling and OPTIONS route for /movies

--- app.js
+++ app.js
@@ -7,7 +7,18 @@ const app = express()
 app.use(express.json())
 app.disable('x-powered-by')
 
+const ACCEPTED_ORIGINS = [
+  'http://localhost:1234',
+  'http://localhost:8080',
+  'http://movies.com',
+  'http://manuelscl.com'
+]
+
 app.get('/movies', (req, res) => {
+  const origin = req.header('origin')
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header('Access-Control-Allow-Origin', origin)
+  }
   const { genre } = req.query
   if (genre) {
     const filteredMovies = movies.filter(
@@ -60,6 +71,15 @@ app.patch('/movies/:id', (req, res) => {
   movies[movieIndex] = updateMovie
 
   return res.json(updateMovie)
+})
+
+app.options('/movies/:id', (req, res) => {
+  const origin = req.header('origin')
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header('Access-Control-Allow-Origin', origin)
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+  }
+  return res.send(200)
 })
 
 const PORT = process.env.PORT ?? 1234
