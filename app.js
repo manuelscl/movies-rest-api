b79eaab4aea244f267e9a3d282b258e436893fbd
manuelscl <manuelscl.dev@gmail.com>
2025-10-08 18:07:36 -0600

    feat: install and configure CORS middleware

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 const express = require('express')
 const crypto = require('node:crypto')
+const cors = require('cors')
 const movies = require('./movies.json')
 const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
 const app = express()
 app.use(express.json())
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:1234',
+      'http://localhost:8080',
+      'http://movies.com',
+      'http://manuelscl.com'
+    ]
+
+    if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+}))
 app.disable('x-powered-by')
 
-const ACCEPTED_ORIGINS = [
-  'http://localhost:1234',
-  'http://localhost:8080',
-  'http://movies.com',
-  'http://manuelscl.com'
-]
-
 app.get('/movies', (req, res) => {
-  const origin = req.header('origin')
-  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
-    res.header('Access-Control-Allow-Origin', origin)
-  }
   const { genre } = req.query
   if (genre) {
     const filteredMovies = movies.filter(
@@ -54,10 +64,6 @@ app.post('/movies', (req, res) => {
 })
 
 app.delete('/movies/:id', (req, res) => {
-  const origin = req.header('origin')
-  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
-    res.header('Access-Control-Allow-Origin', origin)
-  }
   const { id } = req.params
   const movieIndex = movies.findIndex(movie => movie.id === id)
   if (movieIndex === -1) res.status(404).json({ message: 'Movie not found' })
@@ -85,15 +91,6 @@ app.patch('/movies/:id', (req, res) => {
   movies[movieIndex] = updateMovie
 
   return res.json(updateMovie)
-})
-
-app.options('/movies/:id', (req, res) => {
-  const origin = req.header('origin')
-  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
-    res.header('Access-Control-Allow-Origin', origin)
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
-  }
-  return res.send(200)
 })
 
 const PORT = process.env.PORT ?? 1234
